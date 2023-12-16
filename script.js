4a124328fb599bbda8dd6391f864f7e76e13e5aa
Smruti Rani <smrutirani0@gmail.com>
2023-12-17 00:23:24 +0530

    changed

--- script.js
+++ script.js
@@ -3,9 +3,31 @@ const listContainer = document.getElementById("list-container");
 
 function addTask(){
     if(inputBox.value === ''){
-
+        alert("You must write something!")
     }
     else{
-        let li = document.createElement()
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
+    inputBox.value = "";
+    saveDate();
 }
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "SPAN"){
+        e.target.parentelement.remove();
+        saveDate();
+    }
+}, false);
+
+function saveDate(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
